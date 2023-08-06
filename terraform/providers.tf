fd7346f5995f83457f7192ac41d1293b287e2600
devin-gar47 <devingar18@gmail.com>
2023-08-05 21:11:59 -0500

    fix: fixed terraform

---
+++ terraform/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "remote" {
+    organization = "looney-develops"
+    workspaces {
+      name = "model-portal"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
