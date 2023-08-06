693930b11977aa2770d155228414eba887a2414f
devin-gar47 <devingar18@gmail.com>
2023-08-05 20:04:27 -0500

    feat: added configuration for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    include: ['**/*.test.tsx'],
+    globals: true,
+    coverage: {
+      all: true,
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.tsx']
+    },
+  },
+});
